24d291a2dcd4b54a8394cd30842af7e5c12c84fd
MoskalykA <100430077+MoskalykA@users.noreply.github.com>
2023-04-08 23:12:18 +0200

    feat: add data support for signed numbers

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,15 @@ use std::{
 
 pub enum Data {
     String(String),
+    I8(i8),
     U8(u8),
+    I16(i16),
     U16(u16),
+    I32(i32),
     U32(u32),
+    I64(i64),
     U64(u64),
+    I128(i128),
     U128(u128),
 }
 
@@ -35,10 +40,15 @@ impl Collection {
 
             match data {
                 Data::String(string) => cursor.write_string::<u8>(string.to_string()).unwrap(),
+                Data::I8(number) => cursor.write_i8(*number).unwrap(),
                 Data::U8(number) => cursor.write_u8(*number).unwrap(),
+                Data::I16(number) => cursor.write_i16(*number).unwrap(),
                 Data::U16(number) => cursor.write_u16(*number).unwrap(),
+                Data::I32(number) => cursor.write_i32(*number).unwrap(),
                 Data::U32(number) => cursor.write_u32(*number).unwrap(),
+                Data::I64(number) => cursor.write_i64(*number).unwrap(),
                 Data::U64(number) => cursor.write_u64(*number).unwrap(),
+                Data::I128(number) => cursor.write_i128(*number).unwrap(),
                 Data::U128(number) => cursor.write_u128(*number).unwrap(),
             }
         }
